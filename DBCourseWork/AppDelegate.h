c7f93f1f05ef9633771ae03f1ca92e80ac67508e
Victor Shamanov <wiruzx@gmail.com>
2013-10-28 15:13:27 +0400

    Initial Commit

---
+++ DBCourseWork/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  DBCourseWork
+//
+//  Created by Виктор Шаманов on 10/28/13.
+//  Copyright (c) 2013 Victor Shamanov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ DBCourseWork/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DBCourseWork
+//
+//  Created by Виктор Шаманов on 10/28/13.
+//  Copyright (c) 2013 Victor Shamanov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
