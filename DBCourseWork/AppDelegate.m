ccb38234c0a1d2e651b02f4b911b96558eb7b508
Victor Shamanov <wiruzx@gmail.com>
2013-10-29 14:10:18 +0400

    CoreData store type changed from XML to SQLite

--- DBCourseWork/AppDelegate.m
+++ DBCourseWork/AppDelegate.m
@@ -83,7 +83,7 @@
     
     NSURL *url = [applicationFilesDirectory URLByAppendingPathComponent:@"DBCourseWork.storedata"];
     NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
-    if (![coordinator addPersistentStoreWithType:NSXMLStoreType configuration:nil URL:url options:nil error:&error]) {
+    if (![coordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:url options:nil error:&error]) {
         [[NSApplication sharedApplication] presentError:error];
         return nil;
     }
